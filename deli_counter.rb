f2580f0ad986120678fcb531b396962b57a61274
Joseph Martin <josephmartin1350@gmail.com>
2022-02-21 22:30:38 -0700

    commit tree 8fea39a4ac3e6459ea0dec20858d901e9cc69e66

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def line line
+    if line.length == 0
+        puts "The line is currently empty."
+    else
+        current_line = "The line is currently:"
+        line.each.with_index(1) { |name, i| current_line << " #{i}. #{name}" }
+        puts current_line
+    end
+end
+
+def take_a_number(line, name)
+    line << name
+    puts "Welcome, #{name}. You are number #{line.length} in line."
+end
+
+def now_serving line
+    if line.empty?
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{line.first}."
+        line.shift
+    end
+end
